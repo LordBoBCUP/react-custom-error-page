6e036784442f6f18963d8c3c0da6e57a29219a7d
Alex Massey <Alex.Massey@augensoftwaregroup.com>
2019-04-16 16:51:03 +1200

    update logic

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,24 +12,28 @@ class Header extends Component {
     }
     render() {
         const path = this.props.location.pathname.slice(1);
-        if (path === '404') {
-            return (
-                <div>
-                    <h2>404 - Page not found</h2>
-                    <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-                </div>
-            );
-        }
-
-        if (path === '403') {
-            return (
-                <div>
-                    <h2>403 - Access Denied</h2>
-                    <p>Sorry the page you are trying to access is not available.</p>
-                </div>
-            );
-        }
         switch (path) {
+            case '':
+                return (
+                    <div>
+                        <h2>404 - Page not found</h2>
+                        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+                    </div>
+                );
+            case '403':
+                return (
+                    <div>
+                        <h2>403 - Access Denied</h2>
+                        <p>Sorry the page you are trying to access is not available.</p>
+                    </div>
+                );
+            case '404':
+                return (
+                    <div>
+                        <h2>404 - Page not found</h2>
+                        <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+                    </div>
+                );
             case '500':
                 return (
                     <div>
